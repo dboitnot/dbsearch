495abec1fc2ade4efc01530dabba21bce1787b99
Daniel Boitnott <dboitnot@gmail.com>
2019-08-10 14:19:12 -0500

    made loaderForClass() package-private

--- src/main/java/dbsearch/FormLoader.java
+++ src/main/java/dbsearch/FormLoader.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  * Created by dboitnot on 12/29/13.
  */
 public class FormLoader {
-    public static FXMLLoader loaderForClass(Class cls) throws IOException {
+    static FXMLLoader loaderForClass(Class cls) throws IOException {
         String path = "resources/" + cls.getSimpleName() + ".fxml";
         FXMLLoader loader = new FXMLLoader(cls.getResource(path));
         loader.load(cls.getResourceAsStream(path));
